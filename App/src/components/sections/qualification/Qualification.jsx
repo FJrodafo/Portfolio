ea18bed7ff6e0a487899737adc8fc4ef4040ddc4
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-05-18 23:34:22 +0100

    Update Qualification/Translations

--- App/src/components/sections/qualification/Qualification.jsx
+++ App/src/components/sections/qualification/Qualification.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from '@/components/context/translation/Translation.jsx
 const Qualification = () => {
   const { t } = useTranslation();
 
-  const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState(2);
   const toggleTab = (index) => setToggleState(index);
 
   return (
@@ -15,19 +15,21 @@ const Qualification = () => {
       <div className='qualification__container container'>
         <div className='qualification__tabs'>
           <div className={toggleState === 1 ? 'qualification__button qualification__active button--flex' : 'qualification__button button--flex'} onClick={() => toggleTab(1)}>
-            <i className='uil uil-graduation-cap qualification__icon' />{t('qualification.education')}
+            <i className='uil uil-graduation-cap qualification__icon' />{t('qualification.education.header')}
           </div>
           <div className={toggleState === 2 ? 'qualification__button qualification__active button--flex' : 'qualification__button button--flex'} onClick={() => toggleTab(2)}>
-            <i className='uil uil-briefcase-alt qualification__icon' />{t('qualification.experience')}
+            <i className='uil uil-briefcase-alt qualification__icon' />{t('qualification.experience.header')}
           </div>
         </div>
         <div className='qualification__sections'>
           <div className={toggleState === 1 ? 'qualification__content qualification__content-active' : 'qualification__content'}>
             <div className='qualification__data'>
               <div>
-                <h3 className='qualification__title'>{t('qualification.title.iespc')}</h3>
-                <span className='qualification__subtitle'>IES Puerto de la Cruz - Telesforo Bravo</span>
-                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> 2022 - 2026</div>
+                <h3 className='qualification__title'>{t('qualification.education.title.iespc')}</h3>
+                <a href='https://www3.gobiernodecanarias.org/medusa/edublog/iespuertodelacruztelesforobravo/' className='qualification__link' target='_blank' rel='noreferrer'>
+                  <span className='qualification__subtitle'>IES Puerto de la Cruz - Telesforo Bravo</span>
+                </a>
+                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> Oct 2022 - Jun 2026</div>
               </div>
               <div>
                 <span className='qualification__rounder'></span>
@@ -41,16 +43,20 @@ const Qualification = () => {
                 <span className='qualification__line'></span>
               </div>
               <div>
-                <h3 className='qualification__title'>{t('qualification.title.csmc')}</h3>
-                <span className='qualification__subtitle'>CSMC - Conservatorio Superior de Música de Canarias</span>
-                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> 2020 - 2025</div>
+                <h3 className='qualification__title'>{t('qualification.education.title.csmc')}</h3>
+                <a href='https://www3.gobiernodecanarias.org/medusa/edublog/csmcanarias/' className='qualification__link' target='_blank' rel='noreferrer'>
+                  <span className='qualification__subtitle'>CSMC - Conservatorio Superior de Música de Canarias (Sede Tenerife)</span>
+                </a>
+                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> Sep 2020 - Sep 2025</div>
               </div>
             </div>
             <div className='qualification__data'>
               <div>
-                <h3 className='qualification__title'>{t('qualification.title.cpmt')}</h3>
-                <span className='qualification__subtitle'>CPMT - Conservatorio Profesional de Música de Tenerife</span>
-                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> 2018 - 2021</div>
+                <h3 className='qualification__title'>{t('qualification.education.title.cpmt')}</h3>
+                <a href='https://www3.gobiernodecanarias.org/medusa/edublog/cpmsantacruzdetenerife/' className='qualification__link' target='_blank' rel='noreferrer'>
+                  <span className='qualification__subtitle'>CPMT - Conservatorio Profesional de Música de Santa Cruz de Tenerife</span>
+                </a>
+                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> Sep 2018 - Jun 2021</div>
               </div>
               <div>
                 <span className='qualification__rounder'></span>
@@ -62,26 +68,30 @@ const Qualification = () => {
               <div></div>
               <div>
                 <span className='qualification__rounder'></span>
-                {/*<span className='qualification__line'></span>*/}
+                <span className='qualification__line'></span>
               </div>
               <div>
-                <h3 className='qualification__title'>...</h3>
-                <span className='qualification__subtitle'>...</span>
-                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> ... - {t('qualification.present')}</div>
+                <h3 className='qualification__title'>{t('qualification.experience.title.oft')}</h3>
+                <a href='https://www.facebook.com/orquestafilarmonicadetenerife/' className='qualification__link' target='_blank' rel='noreferrer'>
+                  <span className='qualification__subtitle'>OFT - Orquesta Filarmónica de Tenerife</span>
+                </a>
+                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> May 2018 - {t('qualification.present')}</div>
               </div>
             </div>
-            {/*
             <div className='qualification__data'>
               <div>
-                <h3 className='qualification__title'>Backend Developer</h3>
-                <span className='qualification__subtitle'>...</span>
-                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> 2024 - Present</div>
+                <h3 className='qualification__title'>{t('qualification.experience.title.ofjtmj')}</h3>
+                <a href='https://www.facebook.com/OrquestaFilarmonicaJuvenildeTenerifeMiguelJaubert/' className='qualification__link' target='_blank' rel='noreferrer'>
+                  <span className='qualification__subtitle'>OFTMJ - Orquesta Filarmónica Juvenil de Tenerife "Miguel Jaubert"</span>
+                </a>
+                <div className='qualification__calender'><i className='uil uil-calendar-alt' /> Nov 2017 - Feb 2019</div>
               </div>
               <div>
                 <span className='qualification__rounder'></span>
-                <span className='qualification__line'></span>
+                {/*<span className='qualification__line'></span>*/}
               </div>
             </div>
+            {/*
             <div className='qualification__data'>
               <div></div>
               <div>
