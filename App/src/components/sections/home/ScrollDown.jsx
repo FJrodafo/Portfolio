b3b2f34fd463cbf8da902a5d0c63492c02fa8f6d
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-01-16 14:16:14 +0000

    Optimize SVG

--- App/src/components/sections/home/ScrollDown.jsx
+++ App/src/components/sections/home/ScrollDown.jsx
@@ -7,41 +7,21 @@ const ScrollDown = () => {
   return (
     <div className='home__scroll'>
       <a href='#about' className='home__scroll-button button--flex'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          xmlnsXlink='http://www.w3.org/1999/xlink'
-          className='home__scroll-mouse'
-          height='32px'
-          width='32px'
-          viewBox='0 0 247 390'
-          version='1.1'
-          style={{
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" className="home__scroll-mouse" style={{
             fillRule: 'evenodd',
             clipRule: 'evenodd',
             strokeLinecap: 'round',
             strokeLinejoin: 'round',
             strokeMiterlimit: '1.5'
-          }}
-        >
-          <path
-            className='wheel'
-            d='M123.359,79.775l0,72.843'
-            style={{
+          }} viewBox="0 0 247 390" xmlnsXlink="http://www.w3.org/1999/xlink"><path d="M123.359 79.775v72.843" className="wheel" style={{
               fill: 'none',
               stroke: 'var(--title-color)',
               strokeWidth: '20px',
-            }}
-          />
-          <path
-            id='mouse'
-            d='M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z'
-            style={{
+            }}/><path id="mouse" d="M236.717 123.359C236.717 60.794 185.923 10 123.359 10S10 60.794 10 123.359v143.237c0 62.565 50.794 113.359 113.359 113.359s113.358-50.794 113.358-113.359z" style={{
               fill: 'none',
               stroke: 'var(--title-color)',
               strokeWidth: '20px'
-            }}
-          />
-        </svg>
+            }}/></svg>
         <span className='home__scroll-name'>{t('home.scroll')}</span>
         <i className='uil uil-arrow-down home__scroll-arrow' />
       </a>
